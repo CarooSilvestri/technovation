8de9fb71313adba1bbf9e3ed951e0bed622d15bc
Carolina Silvestri <mcsilvestri19@gmail.com>
2026-04-13 23:57:24 -0300

    routes, index

--- js/config.js
+++ js/config.js
@@ -3,8 +3,17 @@
   "use strict";
 
   window.LETRO_CONFIG = {
-    STORAGE_KEY: "letritas_perfil",
+    STORAGE_KEY: "letro_profile",
     SPLASH_DELAY_MS: 5000,
+  };
+
+  window.PAGES = {
+    REGISTER: "register.html",
+    HOME: "home.html",
+    PROFILE: "perfil.html",
+    GAME_UNIR_DIBUJOS: "juego-unir-dibujos.html",
+    GAME_UNIR_MAYUSCULAS: "juego-unir-mayusculas.html",
+    GAME_SILABAS: "juego-silabas.html",
   };
 })();
 
